4a2109b7eb80d6d18b8fa31863c903c57a4c64ec
donghun.lee <alexander930828>
2021-06-10 23:53:58 +0900

    rainy

---
+++ 선택정렬.py
@@ -0,0 +1,23 @@
+def find_min_idx(a):
+    n = len(a)
+    min_idx = 0
+    for i in range(1, n):
+        if a[i] < a[min_idx]:
+            min_idx = 1
+    return min_idx
+
+
+# d = [2, 4, 5, 1, 3]
+# print(find_min_idx(d))
+# print(d.pop())
+
+def sel_sort(a):
+    result = []
+    while a:
+        min_idx = find_min_idx(a)
+        value = a.pop(min_idx)
+        result.append(value)
+    return result
+
+d = [2, 4, 5, 1, 3]
+print(sel_sort(d))
